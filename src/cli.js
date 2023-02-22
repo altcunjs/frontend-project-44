132f7aadad02da9323bd3910796dca1c1e7ed871
altcun <molmajs@gmail.com>
2023-02-22 15:36:07 +0300

    update

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export function SayYourName() {
+function SayYourName() {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log('Hello ' + userName + '!');
