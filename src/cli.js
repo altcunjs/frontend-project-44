3c141105d6f8ede6115169f76d4784bdac6f0fbd
altcun <molmajs@gmail.com>
2023-02-22 15:52:20 +0300

    add return

--- src/cli.js
+++ src/cli.js
@@ -4,4 +4,5 @@ export function SayYourName() {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log('Hello, ' + userName + '!');
+  return userName;
 }
