b572fdd8a5baf086b282934ec4ce68b5f83d44ec
altcun <molmajs@gmail.com>
2023-03-01 16:07:31 +0300

    remove isEven & getRandomOperator

--- src/utils.js
+++ src/utils.js
@@ -5,11 +5,3 @@ export const getRandomInRange = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 /* eslint-enable no-param-reassign */
-export const getRandomOperator = () => {
-  const operators = ['+', '-', '*'];
-  const randomIndex = Math.floor(Math.random() * operators.length);
-  const result = operators[randomIndex];
-  return result;
-};
-
-export const isEven = (number) => number % 2 === 0;
